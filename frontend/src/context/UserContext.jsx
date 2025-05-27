0f949d1fe784bb978bf756216f5acf11199e02b3
mohamedmahfouz3 <mohamed181366@feng.bu.edu.eg>
2025-05-28 00:30:55 +0300

    uber10

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { UserContext } from "./UserContext";
+import React, { createContext, useState } from "react";
+
+export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({
